0c20a516545aaf4d152da9bb9e2318692b36bc1f
Yeffri <ysinisterraa@estudiante.uniajc.edu.co>
2025-02-03 20:57:59 +0000

    mi primer programa en JAVA

---
+++ clase1/ejecuetar.java
@@ -0,0 +1,6 @@
+public class ejecuetar {
+    public static void main(String[] args){
+        System.out.println("hola mundo!!!");
+    }
+}
+
---
+++ clase1/ejecuter.java
@@ -0,0 +1,6 @@
+public class ejecuer {
+    public static void main(string[] args){
+        System.out.print(x:"hola mundo!!!");
+    }
+}
+
